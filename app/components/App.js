da0aa0e578bee8099e4a132092d368fff97a5009
Efrat Rosmarin <efratedit@gmail.com>
2018-07-07 23:44:53 -0400

    weather

---
+++ app/components/App.js
@@ -0,0 +1,48 @@
+var React = require('react');
+var ZipCode = require('./ZipCode');
+var Forecast = require('./Forecast');
+var Detail = require('./Detail');
+var ReactRouter = require('react-router-dom');
+var BrowserRouter = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <div className='container'>
+          <Route render={function (props) {
+            return (
+              <div className='navbar'>
+                <h1>Weather</h1>
+                <ZipCode
+                  direction='row'
+                  onSubmitZipcode={function(city){
+                    props.history.push({
+                      pathname: '/forecast',
+                      search: '?city=' + city
+                    });
+                  }}
+                  zipcode={123} />
+              </div>
+            )
+          }} />
+
+          <Route exact path='/' render={function (props) {
+            return (
+              <div className='home-container' style={{backgroundImage: "url('app/images/community.jpg')"}}>
+               
+              </div>
+            )
+          }} />
+
+          <Route path='/forecast' component={Forecast} />
+
+          <Route path='/details/:city' component={Detail} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+module.exports = App;
